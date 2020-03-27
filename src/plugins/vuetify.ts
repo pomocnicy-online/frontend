08eec2f6f700084168434a89562479bd3ad1332f
Sebastian Mazur <sebastian.mazur@skygate.io>
2020-03-27 21:31:23 +0100

    fix: fixed error with wrong imports

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -5,10 +5,10 @@ import Vuetify from "vuetify";
 import "vuetify/dist/vuetify.min.css";
 import colors from "vuetify/lib/util/colors";
 
-import ArrowNext from "@/components/icons/arrow-next";
-import ArrowBack from "@/components/icons/arrow-back";
-import VoiceIcon from "@/components/icons/voice";
-import HeartIcon from "@/components/icons/heart";
+import ArrowNext from "@/components/icons/arrow-next.vue";
+import ArrowBack from "@/components/icons/arrow-back.vue";
+import VoiceIcon from "@/components/icons/voice.vue";
+import HeartIcon from "@/components/icons/heart.vue";
 
 Vue.use(Vuetify);
 
