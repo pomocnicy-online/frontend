e99d106d12909e9646adbd3cb485ab3edc99355a
Grzegorz Bielski <pesiok@gmail.com>
2020-03-28 17:33:18 +0100

    summary supply view

---
+++ src/components/icons/other.vue
@@ -0,0 +1,10 @@
+<template>
+    <svg width="29" height="8" viewBox="0 0 29 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="4" cy="4" r="3.5" stroke="#4A577B" />
+        <circle cx="14" cy="4" r="3.5" stroke="#4A577B" />
+        <circle cx="25" cy="4" r="3.5" stroke="#4A577B" />
+    </svg>
+</template>
+<script lang="ts">
+export default {};
+</script>
