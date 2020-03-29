e693519b93075b5dfd703095caabad3dff3d2237
Marcin Krawczyk <marckraw@icloud.com>
2020-03-29 21:07:56 +0200

    [choose-outlet view] add simple filter by town - in frontend

--- src/views/CanHelp/Outlet.vue
+++ src/views/CanHelp/Outlet.vue
@@ -3,65 +3,73 @@
         <section>
             <v-container>
                 <step-header name="Wybierz placówkę" current="2" outOf="4" />
+                <v-row>
+                    <v-col class="d-flex" cols="6" xs="6" md="3">
+                        <v-select v-model="selectedTown" :items="towns" label="Miasto"></v-select>
+                    </v-col>
+                </v-row>
                 <article class="step-desc step-table">
                     <v-radio-group v-model="outlet.request.requestId">
-                        <v-simple-table>
-                            <template v-slot:default>
-                                <thead>
-                                    <tr>
-                                        <th class="text-left"></th>
-                                        <th class="text-left">Szpital</th>
-                                        <th class="text-left">Zapotrzebowanie</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr v-for="item in outlets" :key="item.name">
-                                        <td>
-                                            <v-radio :key="item.id" :value="item.id"></v-radio>
-                                        </td>
-                                        <td>
-                                            <div>
-                                                <strong>{{ item.legalName }}</strong>
-                                            </div>
-                                            <div>
-                                                {{ item.city }}, ul. {{ item.street }} {{ item.buildingNumber }}
-                                                {{ item.apartmentNumber && `/ ${item.apartmentNumber}` }}
-                                            </div>
-                                        </td>
-                                        <td class="step-table__supplies">
-                                            <div>
-                                                maseczki:
-                                                <strong>{{ item.maskRequestsTotalCount }}</strong>
-                                            </div>
-                                            <div>
-                                                rekawiczki:
-                                                <strong>{{ item.gloveRequestsTotalCount }}</strong>
-                                            </div>
-                                            <div>
-                                                kombinezony:
-                                                <strong>{{ item.suitRequestsTotalCount }}</strong>
-                                            </div>
-                                            <div>
-                                                zakupy ?:
-                                                <strong>{{ item.groceryRequestsTotalCount }}</strong>
-                                            </div>
-                                            <div>
-                                                plyny dezynfekujące:
-                                                <strong>{{ item.disinfectionMeasureRequestsTotalCount }}</strong>
-                                            </div>
-                                            <div>
-                                                inne dezynfekujące materialy:
-                                                <strong>{{ item.otherCleaningMaterialRequestsTotalCount }}</strong>
-                                            </div>
-                                            <div>
-                                                print request ?
-                                                <strong>{{ item.printRequestsTotalCount }}</strong>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </template>
-                        </v-simple-table>
+                        <v-row class="step-table__row-container d-none d-sm-flex">
+                            <v-col cols="2" md="1"></v-col>
+                            <v-col cols="10" md="5">Szpital</v-col>
+                            <v-col cols="10" md="6">Zapotrzebowanie</v-col>
+                        </v-row>
+
+                        <div
+                            class="step-table__row-container"
+                            v-for="item in filteredOutlets"
+                            :key="item.name"
+                        >
+                            <v-row>
+                                <v-col cols="2" md="1">
+                                    <v-radio :key="item.id" :value="item.id"></v-radio>
+                                </v-col>
+                                <v-col class="offset-md-0" cols="10" md="5">
+                                    <div>
+                                        <strong>{{ item.legalName }}</strong>
+                                    </div>
+                                    <div>
+                                        {{ item.city }}, ul. {{ item.street }} {{ item.buildingNumber }}
+                                        {{ item.apartmentNumber && `/ ${item.apartmentNumber}` }}
+                                    </div>
+                                </v-col>
+                                <v-col
+                                    class="step-table__supplies offset-2 offset-md-0"
+                                    cols="10"
+                                    md="6"
+                                >
+                                    <div>
+                                        maseczki:
+                                        <strong>{{ item.maskRequestsTotalCount }}</strong>
+                                    </div>
+                                    <div>
+                                        rekawiczki:
+                                        <strong>{{ item.gloveRequestsTotalCount }}</strong>
+                                    </div>
+                                    <div>
+                                        kombinezony:
+                                        <strong>{{ item.suitRequestsTotalCount }}</strong>
+                                    </div>
+                                    <div>
+                                        zakupy ?:
+                                        <strong>{{ item.groceryRequestsTotalCount }}</strong>
+                                    </div>
+                                    <div>
+                                        plyny dezynfekujące:
+                                        <strong>{{ item.disinfectionMeasureRequestsTotalCount }}</strong>
+                                    </div>
+                                    <div>
+                                        inne dezynfekujące materialy:
+                                        <strong>{{ item.otherCleaningMaterialRequestsTotalCount }}</strong>
+                                    </div>
+                                    <div>
+                                        print request ?
+                                        <strong>{{ item.printRequestsTotalCount }}</strong>
+                                    </div>
+                                </v-col>
+                            </v-row>
+                        </div>
                     </v-radio-group>
                 </article>
                 <!-- render list of supplies here @seba -->
@@ -95,6 +103,9 @@ export default class CanHelpOutlet extends Vue {
         ]
     };
     outlets = [];
+    filteredOutlets = [];
+    towns = ["Wszystkie"];
+    selectedTown = "";
     path = "/api/requests/";
     outletRequest = null;
 
@@ -108,8 +119,23 @@ export default class CanHelpOutlet extends Vue {
         }
     }
 
+    @Watch("outlets", { immediate: true })
+    onOutletsChange(outlets: any) {
+        this.towns = [...this.towns, ...outlets.map((outlet: any) => outlet.city)];
+    }
+
+    @Watch("selectedTown", { immediate: true })
+    onTownsChange(selectedTown: any) {
+        if (selectedTown === "Wszystkie") {
+            this.filteredOutlets = this.outlets;
+        } else {
+            this.filteredOutlets = this.outlets.filter((outlet: any) => outlet.city === selectedTown);
+        }
+    }
+
     async mounted() {
         this.outlets = await this.getAllOutlets();
+        this.filteredOutlets = this.outlets;
     }
 
     getAllOutlets() {
@@ -141,6 +167,13 @@ export default class CanHelpOutlet extends Vue {
 @include step-desc;
 
 .step-table {
+    height: 65vh;
+    overflow-x: hidden;
+    overflow-y: auto;
+    &__row-container {
+        border-bottom: 1px solid #bebbbb;
+    }
+
     td {
         padding: 14px;
     }
